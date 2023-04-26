92b35d850a8f0e16c01cccb825d675d241228eab
gorthegod <jvashist@gmx.de>
2023-04-26 20:41:28 +0200

    Algolia setup done

--- search.js
+++ search.js
@@ -1,9 +1,9 @@
 /* global instantsearch algoliasearch */
 
 app({
-  appId: 'latency',
-  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-  indexName: 'instant_search',
+  appId: 'JOYMYGJ401',
+  apiKey: '09a82c94d83b9c76b1859b30a02c7327',
+  indexName: 'dev_brennholz',
   searchParameters: {
     hitsPerPage: 10,
   },
